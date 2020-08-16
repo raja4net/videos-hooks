3567b727c2d9acbc381b0c13c54a0549ee3f8dd7
Raja Rupinder Singh <rajarupinder@gmail.com>
2020-08-16 19:00:41 +0530

    Added vide iframe & selecting default search term

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
@@ -13,17 +14,35 @@ class App extends React.Component {
               }
           }  
         );
-        this.setState({ videos: response.data.items });
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
+    }
+
+    componentDidMount() {
+        this.onTermSubmit('buildings');
     }
 
     onVideoSelect = (video) => {
-    console.log('From the app:', video);
+        this.setState({ selectedVideo: video });
     }
 
     render() {
-        return <div className="search-bar"><SearchBar onFormSubmit={this.onTermSubmit} />
-            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
-        </div>
+        return (
+            <div className="search-bar"><SearchBar onFormSubmit={this.onTermSubmit} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
